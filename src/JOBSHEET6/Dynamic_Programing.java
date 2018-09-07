c96911e72b163ceb33a9d2c7bf206c9545b69bf9
Asus <Asus@Raka-Rehardsyah>
2018-09-07 11:07:50 +0700

    Tambah Hitung Fibonacci

--- src/JOBSHEET6/Dynamic_Programing.java
+++ src/JOBSHEET6/Dynamic_Programing.java
@@ -1,4 +1,5 @@
 package JOBSHEET6;
+import java.math.BigInteger;
 import java.util.Scanner;
 public class Dynamic_Programing {
 
@@ -15,12 +16,25 @@ public class Dynamic_Programing {
         return 0;
     }
     
+    private static BigInteger fibo(int n){
+        BigInteger[] hasil = new BigInteger[n];
+        
+       hasil[0] = BigInteger.ONE;
+       hasil[1] = BigInteger.ONE;
+       
+       for(int i = 1; i < n; i++){
+           hasil[i] = hasil [i-1].add(hasil[i-1]);
+       }
+       return hasil[n-1];
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
         String identitas = "Dimas Afif Rehardendi / X RPL 6 / 18";
 //        System.out.println(identitas);
         tampilJudul(identitas);
         int n = tampilInput();
+        BigInteger hasil = fibo(n);
 
     }
 }
