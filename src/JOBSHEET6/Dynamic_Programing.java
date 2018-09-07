f3d09a155363714f935ff77532998a67e6a307d0
Asus <Asus@Raka-Rehardsyah>
2018-09-07 09:47:12 +0700

    Dynamic Programing Identitas

---
+++ src/JOBSHEET6/Dynamic_Programing.java
@@ -0,0 +1,18 @@
+package JOBSHEET6;
+
+public class Dynamic_Programing {
+
+    public static void tampilJudul(String identitas) {
+        System.out.println("identitas " + identitas);
+        System.out.println("\nHitung Fibbonaci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21....dts\n");
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String identitas = "Dimas Afif Rehardendi / X RPL 6 / 18";
+//        System.out.println(identitas);
+        tampilJudul(identitas);
+
+    }
+}
