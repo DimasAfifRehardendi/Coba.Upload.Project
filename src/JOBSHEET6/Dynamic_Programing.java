e9e6d0290e66e4e1617228910e49b7b57f43fb93
Asus <Asus@Raka-Rehardsyah>
2018-09-07 11:10:11 +0700

    Tambah Tampil Hasil

--- src/JOBSHEET6/Dynamic_Programing.java
+++ src/JOBSHEET6/Dynamic_Programing.java
@@ -27,6 +27,9 @@ public class Dynamic_Programing {
        }
        return hasil[n-1];
     }
+    private static void tampilHasil(int n, BigInteger hasil){
+        System.out.println("Bilangan Fibonacci Ke- " + n + " : " + hasil);
+    }
     
     public static void main(String[] args) {
         // TODO code application logic here
@@ -35,6 +38,7 @@ public class Dynamic_Programing {
         tampilJudul(identitas);
         int n = tampilInput();
         BigInteger hasil = fibo(n);
+        tampilHasil(n, hasil);
 
     }
 }
