485f1f6818f9d58fca0ba83561bde3aa6717e10f
Asus <Asus@Raka-Rehardsyah>
2018-09-07 11:01:27 +0700

    Dynamic Programing Tambah input

--- src/JOBSHEET6/Dynamic_Programing.java
+++ src/JOBSHEET6/Dynamic_Programing.java
@@ -1,18 +1,26 @@
 package JOBSHEET6;
-
+import java.util.Scanner;
 public class Dynamic_Programing {
 
-    public static void tampilJudul(String identitas) {
+    private static void tampilJudul(String identitas) {
         System.out.println("identitas " + identitas);
         System.out.println("\nHitung Fibbonaci");
         System.out.println("1, 1, 2, 3, 5, 8, 13, 21....dts\n");
     }
-
+    private static int tampilInput(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Bilangan ke-: ");
+        int n = scan.nextInt();
+        
+        return 0;
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
         String identitas = "Dimas Afif Rehardendi / X RPL 6 / 18";
 //        System.out.println(identitas);
         tampilJudul(identitas);
+        int n = tampilInput();
 
     }
 }
